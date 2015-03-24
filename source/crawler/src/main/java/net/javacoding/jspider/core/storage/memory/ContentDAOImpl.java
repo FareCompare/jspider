01f3e7977474984a77c04ccdd34fcb0cdd9c2d9c
Chris Stillwell <chris.stillwell@farecompare.com>
2015-03-24 16:12:40 -0500

    Update ContentDAOImpl to java 7.

--- source/crawler/src/main/java/net/javacoding/jspider/core/storage/memory/ContentDAOImpl.java
+++ source/crawler/src/main/java/net/javacoding/jspider/core/storage/memory/ContentDAOImpl.java
@@ -2,33 +2,32 @@ package net.javacoding.jspider.core.storage.memory;
 
 import net.javacoding.jspider.core.storage.spi.ContentDAOSPI;
 import net.javacoding.jspider.core.storage.spi.StorageSPI;
-import net.javacoding.jspider.core.model.ResourceInternal;
 
-import java.io.InputStream;
 import java.io.ByteArrayInputStream;
-import java.util.Map;
+import java.io.InputStream;
 import java.util.HashMap;
+import java.util.Map;
 
 /**
  * $Id: ContentDAOImpl.java,v 1.2 2003/04/11 16:37:06 vanrogu Exp $
  */
 class ContentDAOImpl implements ContentDAOSPI {
 
-    protected Map contents;
+    protected Map<Integer, byte[]> contents;
     protected StorageSPI storage;
 
     public ContentDAOImpl ( StorageSPI storage ) {
         this.storage = storage;
-        this.contents = new HashMap ( );
+        this.contents = new HashMap<>( );
     }
 
     public InputStream getInputStream(int id) {
-        byte[] bytes = (byte[]) contents.get(new Integer(id));
+        byte[] bytes = contents.get( id );
         return new ByteArrayInputStream ( bytes );
     }
 
     public void setBytes(int id, byte[] bytes) {
-        contents.put(new Integer(id), bytes);
+        contents.put( id, bytes);
     }
 
 }
