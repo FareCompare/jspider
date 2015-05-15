7667b29ad1741346e64721087743d5952189f225
Chris Stillwell <chris.stillwell@farecompare.com>
2015-05-15 08:08:30 -0500

    refactor clearDatabase

--- source/crawler/src/main/java/net/javacoding/jspider/core/storage/jdbc/JdbcStorageImpl.java
+++ source/crawler/src/main/java/net/javacoding/jspider/core/storage/jdbc/JdbcStorageImpl.java
@@ -1,22 +1,44 @@
 package net.javacoding.jspider.core.storage.jdbc;
 
 
-import net.javacoding.jspider.core.storage.spi.*;
-import net.javacoding.jspider.core.util.config.PropertySet;
+import net.javacoding.jspider.core.logging.Log;
 import net.javacoding.jspider.core.logging.LogFactory;
+import net.javacoding.jspider.core.storage.spi.ContentDAOSPI;
+import net.javacoding.jspider.core.storage.spi.CookieDAOSPI;
+import net.javacoding.jspider.core.storage.spi.DecisionDAOSPI;
+import net.javacoding.jspider.core.storage.spi.EMailAddressDAOSPI;
+import net.javacoding.jspider.core.storage.spi.FolderDAOSPI;
+import net.javacoding.jspider.core.storage.spi.ResourceDAOSPI;
+import net.javacoding.jspider.core.storage.spi.SiteDAOSPI;
+import net.javacoding.jspider.core.storage.spi.StorageSPI;
+import net.javacoding.jspider.core.util.config.PropertySet;
 
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 
 /**
  * $Id: JdbcStorageImpl.java,v 1.8 2003/04/11 16:37:06 vanrogu Exp $
  *
  * @author G�nther Van Roey
- *
  * @todo DAO's need caching - definitely !
  * @todo find a good way of cleaning the store at spidering start or layering it per spider session
  */
 public class JdbcStorageImpl implements StorageSPI {
+
+    protected static final String[] TABLES = new String[]{
+            "jspider_content",
+            "jspider_cookie",
+            "jspider_decision",
+            "jspider_decision_step",
+            "jspider_site",
+            "jspider_resource",
+            "jspider_resource_reference",
+            "jspider_email_address",
+            "jspider_email_address_reference",
+            "jspider_folder"
+    };
 
     protected SiteDAOSPI siteDAO;
     protected ResourceDAOSPI resourceDAO;
@@ -27,37 +49,31 @@ public class JdbcStorageImpl implements StorageSPI {
     protected FolderDAOSPI folderDAO;
     private final DBUtil dbUtil;
 
-    public JdbcStorageImpl(PropertySet props) {
-        dbUtil = new DBUtil(props);
-        siteDAO = new SiteDAOImpl(this, dbUtil );
-        resourceDAO = new ResourceDAOImpl(this, dbUtil );
-        contentDAO = new ContentDAOImpl(this, dbUtil );
-        decisionDAO = new DecisionDAOImpl(this, dbUtil );
-        cookieDAO = new CookieDAOImpl(this, dbUtil );
-        emailAddressDAO = new EMailAddressDAOImpl(this, dbUtil );
-        folderDAO = new FolderDAOImpl(this, dbUtil );
-        clearDatabase ( dbUtil );
+    public JdbcStorageImpl( PropertySet props ) {
+        dbUtil = new DBUtil( props );
+        siteDAO = new SiteDAOImpl( this, dbUtil );
+        resourceDAO = new ResourceDAOImpl( this, dbUtil );
+        contentDAO = new ContentDAOImpl( this, dbUtil );
+        decisionDAO = new DecisionDAOImpl( this, dbUtil );
+        cookieDAO = new CookieDAOImpl( this, dbUtil );
+        emailAddressDAO = new EMailAddressDAOImpl( this, dbUtil );
+        folderDAO = new FolderDAOImpl( this, dbUtil );
+        clearDatabase( dbUtil );
     }
 
-    protected void clearDatabase ( DBUtil dbUtil ) {
+    protected void clearDatabase( DBUtil dbUtil ) {
+        Log log = LogFactory.getLog( getClass() );
         try (
                 Connection connection = dbUtil.getConnection();
         ) {
             Statement st = connection.createStatement();
-            st.executeUpdate("truncate jspider_content");
-            st.executeUpdate("truncate jspider_cookie");
-            st.executeUpdate("truncate jspider_decision");
-            st.executeUpdate("truncate jspider_decision_step");
-            st.executeUpdate("truncate jspider_site");
-            st.executeUpdate("truncate jspider_resource");
-            st.executeUpdate("truncate jspider_resource_reference");
-            st.executeUpdate("truncate jspider_email_address");
-            st.executeUpdate("truncate jspider_email_address_reference");
-            st.executeUpdate("truncate jspider_folder");
-        } catch (SQLException e) {
-            LogFactory.getLog(JdbcStorageImpl.class).error("SQLException during emtpy of database", e);
+            for ( String table : TABLES ) {
+                log.info( "Clearing table " + table );
+                st.executeUpdate( "truncate " + table );
+            }
+        } catch ( SQLException e ) {
+            LogFactory.getLog( JdbcStorageImpl.class ).error( "SQLException during emtpy of database", e );
         }
-
     }
 
     public DBUtil getDbUtil() {
