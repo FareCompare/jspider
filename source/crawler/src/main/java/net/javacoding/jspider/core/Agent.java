8068c98467f35b9a89aa0d3b1bfcdf82b9c2b610
Chris Stillwell <chris.stillwell@farecompare.com>
2015-12-03 14:23:51 -0600

    code cleanup

--- source/crawler/src/main/java/net/javacoding/jspider/core/Agent.java
+++ source/crawler/src/main/java/net/javacoding/jspider/core/Agent.java
@@ -14,49 +14,49 @@ import java.net.URL;
  *
  * $Id: Agent.java,v 1.5 2003/03/27 17:44:01 vanrogu Exp $
  *
- * @author Günther Van Roey
+ * @author Gï¿½nther Van Roey
  */
 public interface Agent {
 
-    public void start ( );
+    void start();
 
     /**
      * Registers that a certain taks is completed.
      * @param task the Thinker task that is completed
      */
-    public void flagDone ( WorkerTask task );
+    void flagDone( WorkerTask task );
 
     /**
      * Returns a thinker task that must be executed
      * @return the task to be carried out
      * @throws net.javacoding.jspider.core.exception.TaskAssignmentException if there are no tasks (for now or anymore)
      */
-    public WorkerTask getThinkerTask() throws TaskAssignmentException;
+    WorkerTask getThinkerTask() throws TaskAssignmentException;
 
     /**
      * Returns a spider task that must be executed
      * @return the task to be carried out
      * @throws net.javacoding.jspider.core.exception.TaskAssignmentException if there are no tasks (for now or anymore)
      */
-    public WorkerTask getSpiderTask() throws TaskAssignmentException;
+    WorkerTask getSpiderTask() throws TaskAssignmentException;
 
     /**
      * Asks to schedule spidering on a certain URL.
      * @param url the URL to be spidered
      */
-    public void scheduleForSpidering ( URL url );
+    void scheduleForSpidering( URL url );
 
     /**
      * Asks to schedule parsing for a certain fetched URL.
      * @param url the URL to be spidered
      */
-    public void scheduleForParsing ( URL url );
+    void scheduleForParsing( URL url );
 
     /**
      * Notifies the agent of a certain event.
      * @param url the URL that was being processed while the event was raised
      * @param event the event that was raised
      */
-    public void registerEvent(URL url, CoreEvent event);
+    void registerEvent( URL url, CoreEvent event );
 
 }
